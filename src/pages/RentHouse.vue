4aee20e96e454fbbc1076e70a4df36487b072295
travelerzz <251744138@qq.com>
2017-02-26 00:20:41 +0800

    zz

--- src/pages/RentHouse.vue
+++ src/pages/RentHouse.vue
@@ -6,6 +6,9 @@
     //-   input.validate(type="text" v-model='basicData.rcName' placeholder='')
     //-   label.active 人才姓名
     .input-field.col.s12
+      input.validate(type="text" v-model='basicData.poId' placeholder='' v-bind:disabled="disabled_edit"  ,v-bind:class="{ 'vf-invalid-required': startvlwc&&!basicData.poId }")
+      label.active 租房补贴标识
+    .input-field.col.s12
       input.validate(type="text" v-model='basicData.poXm' placeholder='' v-bind:disabled="disabled_edit"  ,v-bind:class="{ 'vf-invalid-required': startvlwc&&!basicData.poXm}")
       label.active 配偶姓名
     .col.s12
@@ -35,8 +38,118 @@
     .input-field.col.s12
       input.validate(type="text" v-model='basicData.dzyj' placeholder='' v-bind:disabled="disabled_edit")
       label.active 邮箱
+    .col.s12
+      label.active 申请类别
+      v-select(:value.sync='basicData.sqlb', :options='sqlb' v-bind:disabled="disabled_edit"  ,v-bind:class="{ 'vf-invalid-required': startvlwc&&!basicData.sqlb }")
+    .input-field.col.s12
+      label.active 申请人及配偶在本地自有住房和享受政策情况说明
+      textarea.validate(type="text" rows='10' cols='30' v-model='basicData.remark' placeholder='' v-bind:disabled="disabled_edit")
+    .input-field.col.s12
+      input.validate(type="text" v-model='basicData.yhzh' placeholder='' v-bind:disabled="disabled_edit"  ,v-bind:class="{ 'vf-invalid-required': startvlwc&&!basicData.yhzh }")
+      label.active 申请人银行账号及开户行
+    .input-field.col.s12
+      input.validate(type="text" v-model='basicData.gzMonth' placeholder='如：3' v-bind:disabled="disabled_edit"  ,v-bind:class="{ 'vf-invalid-required': startvlwc&&!basicData.gzMonth }")
+      label.active 工作月数
+    .col.s12
+      label.active 补贴标准（元/月）
+      input.validate(readonly="readonly" v-model='basicData.btbz' placeholder='' value='200/人/月' v-bind:disabled="disabled_edit"  ,v-bind:class="{ 'vf-invalid-required': startvlwc&&!basicData.btbz }")
+    .input-field.col.s12
+      input.validate(type="text" readonly="readonly" v-model='basicData.btje' placeholder='' v-bind:disabled="disabled_edit"  ,v-bind:class="{ 'vf-invalid-required': startvlwc&&!basicData.btje }")
+      label.active 补贴金额
+    h6 其他成员信息(直系)
+    table
+      thead
+        th 姓名
+        th 性别
+        th 出生年月
+        th 身份证(护照)
+        th 单位信息
+        th 删除
+      tbody
+        tr(v-for='item in myChildren' )
+          td(@click='modal($index)') {{item.name}}
+          td(@click='modal($index)') {{item.sex === "1" ? "男" : "女" }}
+          td(@click='modal($index)') {{item.birthday}}
+          td(@click='modal($index)') {{item.idCard}}
+          td(@click='modal($index)') {{item.dwInfo}}
+          td(@click='deletechild($index)' class='btn waves-effect waves-light red') 删除
+    a(class='btn waves-effect waves-light red', @click='modal')
+      i.fa.fa-plus
+    div.modal#modal1.bottom-sheet
+      div.modal-dialog(style='padding: 20px')
+        div.modal-content
+          div.modal-header
+          div.modal-body()
+            div.form-group
+              div.col-lg-10
+                input(type="text" class="form-control" placeholder="姓名" v-model="child.name" ,v-bind:class="{ 'vf-invalid-required': startvl&&!child.name }")
+            .col.s12(style="padding:0;")
+              label.active 性别
+              v-select(:value.sync='child.sex', :options='xb'  ,v-bind:class="{ 'vf-invalid-required': startvl&&!child.sex }")
+            div.form-group
+              div.col-lg-10(style="padding:0;")
+                label.active 出生年月
+                input(type="month" class="form-control" placeholder="出生年月" v-model="child.birthday" ,v-bind:class="{ 'vf-invalid-required': startvl&&!child.birthday }")
+            div.form-group
+              div.col-lg-10
+                input(type="text" class="form-control" placeholder="身份证(护照)" v-model="child.idCard" ,v-bind:class="{ 'vf-invalid-required': startvl&&!child.idCard }")
+            div.form-group
+              div.col-lg-10
+                input(type="text" class="form-control" placeholder="单位信息" v-model="child.dwInfo" )
+            div.form-group
+              div.col-lg-10
+                input(type="text" class="form-control" placeholder="与申请人关系" v-model="child.cygx" ,v-bind:class="{ 'vf-invalid-required': startvl&&!child.cygx }")
+          div.modal-footer
+            a(class="btn waves-effect waves-green" v-on:click="addChild" ) 确认
+            a(class="modal-action modal-close waves-effect waves-green btn-flat") 取消
+    br
+    br
+    .col.sl2
+    .card
+      .card-title 租赁合同
+      .card-content(v-show='!cacheFile||!cacheFile.length||cacheFile.length==0') 暂无数据
+      div(v-for='item in cacheFile')
+        .card-content(v-if='item.fileType==".jpg"||item.fileType==".png"||item.fileType==".jpeg"||item.fileType==".bmp"||item.fileType==".gif"')
+          img(style='width: 100%;'  v-bind:src='getSrc(item.fileId)')
+        .card-content(v-if='!(item.fileType==".jpg"||item.fileType==".png"||item.fileType==".jpeg"||item.fileType==".bmp"||item.fileType==".gif")')
+          span {{item.fileName}}{{item.fileType}}下载连接（请复制到浏览器中打开）
+            a(style='word-wrap:break-word;') {{getSrc(item.fileId)}}
+        a(@click='deleteFile(item)') 删除
+        .card-action
 
-    button.waves-effect.waves-light.btn(@click='submitData' v-if='!disabled_edit') 保存
+    .card
+      .card-title 公司提供无住房证明
+      .card-content(v-show='!cacheFile2||!cacheFile2.length||cacheFile2.length==0') 暂无数据
+      div(v-for='item in cacheFile2')
+        .card-content(v-if='item.fileType==".jpg"||item.fileType==".png"||item.fileType==".jpeg"||item.fileType==".bmp"||item.fileType==".gif"')
+          img(style='width: 100%;'  v-bind:src='getSrc(item.fileId)')
+        .card-content(v-if='!(item.fileType==".jpg"||item.fileType==".png"||item.fileType==".jpeg"||item.fileType==".bmp"||item.fileType==".gif")')
+            span {{item.fileName}}{{item.fileType}}下载连接（请复制到浏览器中打开）
+              a(style='word-wrap:break-word;') {{getSrc(item.fileId)}}
+        a(@click='deleteFile(item)') 删除
+        .card-action
+
+  .btn.btn-up(v-if='!disabled_edit')
+      .card-title(style='color:black;text-align:left;') 租赁合同
+      .fileupload-button(@click='uploadImg("ZLHT")') 图片文件
+      .fileupload-button(style="line-height: 0 !important;")
+        div(style="margin-top: 15px;") 其他类型文件
+          form(id="uploadForm" enctype="multipart/form-data")
+            input(style="margin-top: 10px;" id="ZLHTuploadInput" type="file")
+            .waves-effect.waves-light.btn(style="width: 100%;margin-top: 10px;" @click='uploadFile("ZLHT")')上传
+  .btn.btn-up(style='margin-top: 140px;' v-if='!disabled_edit')
+      .card-title(style='color:black;text-align:left;') 公司提供无住房证明
+      .fileupload-button(@click='uploadImg("WZFZM")') 图片文件
+      .fileupload-button(style="line-height: 0 !important;")
+        div(style="margin-top: 15px;") 其他类型文件
+          form(id="uploadForm" enctype="multipart/form-data")
+            input(style="margin-top: 10px;" id="WZFZMuploadInput" type="file")
+            .waves-effect.waves-light.btn(style="width: 100%;margin-top: 10px;" @click='uploadFile("WZFZM")')上传
+  .col.s12(v-for='src in media' v-if='!disabled_edit')
+      .card
+        .card-image
+          img(v-bind:src='src', style='width: 100%')
+    button.waves-effect.waves-light.btn(style="width: 100%;margin-top: 140px;" @click='submitData' v-if='!disabled_edit') 保存
 </template>
 <script>
 import rest from '../rest'
@@ -47,8 +160,11 @@ import VLoading from './VLoading.vue'
 import sha1 from 'sha1'
 import _ from 'lodash'
 import { getData } from '../vuex/getters'
+import { uploadImage } from '../rest'
+import { chooseImage } from '../rest'
 
 var localStorage = window.localStorage
+var poId = randomToken(32)
 
 export default{
   vuex: {
@@ -77,7 +193,9 @@ export default{
       zgbmId: [],
       ryId: {},
       basicData: {
-        xb:''
+        xb:'',
+        btje:'',
+        gzMonth:''
       },
       list: [],
       files:[],
@@ -116,6 +234,34 @@ export default{
         },
         responseType: 'json',
         withCredentials: false
+      },
+      reqoptsZXHKB: {
+        alias: 'fileData',
+        formData: {
+          'Encoding': 'utf-8',
+          'Rpencoding': 'utf-8',
+          '_x-requested-with': true,
+          'ssoOpenId': this.user.ssoOpenId,
+          'rcId': this.user.rcId,
+          // zxId: zxId,
+          'useType': 'ZLHT'
+        },
+        responseType: 'json',
+        withCredentials: false
+      },
+      reqoptsZXCSZ: {
+        alias: 'fileData',
+        formData: {
+          'Encoding': 'utf-8',
+          'Rpencoding': 'utf-8',
+          '_x-requested-with': true,
+          'ssoOpenId': this.user.ssoOpenId,
+          'rcId': this.user.rcId,
+          // zxId: zxId,
+          'useType': 'WZFZM'
+        },
+        responseType: 'json',
+        withCredentials: false
       }
     }
   },
@@ -123,8 +269,12 @@ export default{
     this.user = JSON.parse(localStorage.getItem('baseInfo'))
 
     var me = this
+    poId = randomToken(32)
     rest.getOptions('rcrd_cengci').then(res => {
       me.rclb = this.rebuildOptions(res)
+    })
+    rest.getOptions('rczfbz_sqlb').then(res => {
+      me.sqlb = this.rebuildOptions(res)
     })
   },
   ready () {
@@ -133,6 +283,12 @@ export default{
     if (this.dataValue && this.$route.query.do)
       {
           this.basicData = this.dataValue
+          rest.post(this.user, {poId: this.dataValue.poId}, '/rccore/PoxxQtcy/list').then(res => {
+        res.datas.forEach(v => {
+          v.saveInDatebase = 'false'
+        })
+        this.myChildren  = res.datas
+      })
           if(this.$route.query.do=='edit'){
               this.disabled_edit=false;
           }else{ this.disabled_edit=true;
@@ -142,6 +298,7 @@ export default{
           }else{
             this.basicData.xb='0';
           }
+       this.getFileList();
       }
       this.startvlwc=false;
   },
@@ -149,15 +306,116 @@ export default{
     $('#sidenav-overlay').remove()
 
   },
+	watch:{
+		'basicData.gzMonth':function(){
+			
+			if(this.basicData.gzMonth){
+				this.basicData.btje = parseFloat(this.basicData.gzMonth) * parseFloat(this.basicData.btbz)
+			}else if(this.basicData.gzMonth.innerHTML==null){
+				this.basicData.btje = ''
+			}
+		}
+	},
   methods: {
     fileUploadUrl (useType) {
       return rest.basicUrl + '/rccore/RcpoFile/insert' + this.beforeUpload(useType)
     },
     addChild() {
-      this.myChildren.push(this.child)
-      $('#modal1').closeModal()
+//    this.myChildren.push(this.child)
+    if(!this.startvl){
+      this.startvl=true;
+    }
+    if(!this.child.name||!this.child.sex||!this.child.birthday||!this.child.idCard||!this.child.cygx){
+      return;
+    }
+     this.startvl=false;
+      if(!this.child.cyId||this.child.cyId==''){
+        this.child.cyId = randomToken(32)
+        this.child.poId = this.basicData.poId || poId
+        this.child.saveInDatebase = true
+        this.myChildren.push(this.child)
 
+      }else{
+        for(var i =0;i<this.myChildren.length;i++){
+          if(this.child.cyId==this.myChildren[i].cyId){
+            this.myChildren[i]=this.child;
+            break;
+          }
+        }
+      }
+      $('#modal1').closeModal()
       this.child = {}
+			
+    },
+    deletechild(index){
+
+     this.myChildren.splice(index, 1);
+    },
+    uploadFile(useType){
+     let vm = this
+        if(this.disabled_edit)return;
+        let query = {
+        'Encoding': 'utf-8',
+        'Rpencoding': 'utf-8',
+        '_x-requested-with': true,
+        'poId': this.basicData.poId || poId,
+        'rcId' : this.user.rcId,
+        'useType': useType
+      }	
+
+        var file = $('#'+useType+'uploadInput')[0].files[0];
+        var formData = new FormData();
+        formData.append(file.name,file);
+        vm.loading = true;
+        rest.postFile2(vm.user, query, formData,'/rccore/PoxxFile/insert')
+        .then(res => {
+            //alert(JSON.stringify(res));
+            if (!res.success) return Materialize.toast(res.message, 4000)
+            vm.getFileList();
+            vm.media=[];
+            vm.loading = false
+        });
+    },
+    uploadImg (useType) {
+      if(this.disabled_edit)return;
+      let formData = {
+        'Encoding': 'utf-8',
+        'Rpencoding': 'utf-8',
+        '_x-requested-with': true,
+        'poId': this.basicData.poId || poId,
+        'rcId' : this.user.rcId,
+        'useType': useType
+      }
+      let vm = this
+      rest.resetConfig(window.location.href,function(){
+        chooseImage()
+        .then(localId => {
+          vm.loading = true
+          vm.media.push(localId)
+          return uploadImage(localId)
+        })
+        .then(serverId => {
+          return rest.postFile(vm.user, formData, serverId, '/rccore/PoxxFile/insert')
+        })
+        .then(res => {
+          if (!res.success) return Materialize.toast(res.message, 4000)
+          vm.media=[];
+          vm.getFileList();
+          vm.loading = false
+        })
+      })
+    },
+    deleteFile(item) {
+      this.loading = true
+      rest.post(this.user, {refId: item.fileId}, '/rccore/PoxxFile/delete').then(res => {
+        this.loading = false
+
+        if (!res.success) return Materialize.toast(res.message, 4000)
+        this.getFileList();
+      })
+    },
+		closeModal() {
+      $('#modal1').closeModal()
     },
     beforeUpload (useType) {
       var now = Date.now()
@@ -223,7 +481,7 @@ export default{
        if(!this.startvlwc){
           this.startvlwc=true;
       }
-      if(!this.basicData.poXm||!this.basicData.xb||!this.basicData.csny||!this.basicData.jg ||!this.basicData.sfz||!this.basicData.sqcc||!this.basicData.xgzdw||!this.basicData.sjhm){
+      if(!this.basicData.poXm||!this.basicData.xb||!this.basicData.csny||!this.basicData.jg ||!this.basicData.sfz||!this.basicData.sqcc||!this.basicData.xgzdw||!this.basicData.sjhm||!this.basicData.sqlb||!this.basicData.remark||!this.basicData.yhzh||!this.basicData.gzMonth||!this.basicData.btbz||!this.basicData.btje){
         return;
       }
        this.startvlwc=false;
@@ -269,6 +527,13 @@ export default{
       return r
     },
     modal () {
+    if(this.disabled_edit)return;
+      if(typeof index =='number'){
+
+        this.child=this.myChildren[index];
+      }else{
+      this.child={};
+      }
       $('#modal1').openModal()
     },
     getFileList () {
@@ -277,6 +542,12 @@ export default{
       rest.post(this.user, {useType: 'RYZS'}, '/rccore/RcxxFile/fileList').then(res => {
 
         me.fileList = res.datas
+      })
+      rest.post(this.user, {useType: 'ZLHT', poId: this.basicData.poId || poId}, '/rccore/RcxxFile/fileList').then(res => {
+        this.cacheFile2  = res.datas
+      })
+			rest.post(this.user, {useType: 'WZFZM', poId: this.basicData.poId || poId}, '/rccore/RcxxFile/fileList').then(res => {
+        this.cacheFile2  = res.datas
       })
     }
   },
@@ -297,7 +568,7 @@ export default{
   padding-top: 55px;
 }
 .fileupload-button {
-  background-color: #666 !important;
+  /*background-color: #666 !important;*/
   width: 100%;
 }
 .btn-add {
@@ -311,10 +582,28 @@ export default{
 .btn-up {
   width: 100%;
   background-color: transparent;
+  margin-bottom: 60px;
   box-shadow: none;
 }
 .vf-invalid-required{
  border-bottom: 1px solid #F44336 !important;
     box-shadow: 0 1px 0 0 #F44336 !important;
+}
+.card .card-title{
+  font-size:16px;
+}
+table{
+table-layout:fixed;
+}
+table td{
+  word-wrap:break-word;
+}
+.vf-invalid-customValidator{
+  border-bottom: 1px solid #F44336 !important;
+  box-shadow: 0 1px 0 0 #F44336 !important;
+}
+textarea{
+	height: 4em;
+	margin-top: 5px;
 }
 </style>
